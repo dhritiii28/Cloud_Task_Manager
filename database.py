d01a76222e3f2c1a84947f3900904d79d3cefe6b
dhritiii28 <dhritibaidwan@gmail.com>
2026-07-08 17:21:38 +0530

    added .env file

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
+from dotenv import load_dotenv
+load_dotenv()
+# import the function that can read a .env file , i .e open the new file and load all of its variables , into the memory
 
-DATABASE_URL = "postgresql+psycopg2://neondb_owner:npg_AbUk38rsFoCu@ep-aged-pine-aoe987vk-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DATABASE_URL = os.getenv("DATABASE_URL")
 # here, we are connecting to a PostgreSQL database hosted on the cloud (Neon)
 
 # note: click "Show Password" and copy the complete connection string
